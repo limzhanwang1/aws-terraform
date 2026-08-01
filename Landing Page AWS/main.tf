1b74b8b068890bdaf85c5f98497ed8f36fd8efc5
limzhanwang1 <lim.zhanwang@gmail.com>
2026-08-01 19:55:45 +0800

    to enchance landing page

--- Landing Page AWS/main.tf
+++ Landing Page AWS/main.tf
@@ -23,16 +23,16 @@ provider "aws" {
 
 
 # Import the existing S3 Bucket into Terraform state
-import {
-  to = module.landing_page_site.aws_s3_bucket.this
-  id = "landingpage-southlab"
-}
+#import {
+#  to = module.landing_page_site.aws_s3_bucket.this
+#  id = "landingpage-southlab"
+#}
 
 # Import the existing DynamoDB Table into Terraform state
-import {
-  to = module.landing_page_site.aws_dynamodb_table.tf_locks
-  id = "terraform-state-locks"
-}
+#import {
+#  to = module.landing_page_site.aws_dynamodb_table.tf_locks
+#  id = "terraform-state-locks"
+#}
 
 
 
