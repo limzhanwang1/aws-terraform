f64cc27d577b491c94aeebe385022c871fe8a88d
limzhanwang1 <lim.zhanwang@gmail.com>
2026-08-01 22:15:43 +0800

    To create cloudfront

--- Landing Page AWS/modules/cloudfront/main.tf
+++ Landing Page AWS/modules/cloudfront/main.tf
@@ -1,4 +1,9 @@
-# 1. Create Origin Access Control (OAC) for secure private S3 access
+# 1. Look up the official AWS-managed CachingOptimized policy
+data "aws_cloudfront_cache_policy" "caching_optimized" {
+  name = "Managed-CachingOptimized"
+}
+
+# 2. Create Origin Access Control (OAC)
 resource "aws_cloudfront_origin_access_control" "oac" {
   name                              = "${var.s3_bucket_id}-oac"
   description                       = "OAC for ${var.domain_name}"
@@ -7,7 +12,7 @@ resource "aws_cloudfront_origin_access_control" "oac" {
   signing_protocol                  = "sigv4"
 }
 
-# 2. CloudFront Distribution
+# 3. CloudFront Distribution
 resource "aws_cloudfront_distribution" "this" {
   enabled             = true
   is_ipv6_enabled     = true
@@ -26,14 +31,13 @@ resource "aws_cloudfront_distribution" "this" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-${var.s3_bucket_id}"
 
-    # Use AWS Managed Caching Policy (CachingOptimized)
-    cache_policy_id = "65832700-8fa7-417a-7651-509500d10134"
+    # Use the dynamic data source ID instead of a hardcoded string
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_optimized.id
 
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
   }
 
-  # Redirect 403 / 404 to index.html for Single Page Apps (Optional)
   custom_error_response {
     error_code            = 404
     response_code         = 200
@@ -47,35 +51,9 @@ resource "aws_cloudfront_distribution" "this" {
     }
   }
 
-  # ACM Certificate for Custom Subdomain
   viewer_certificate {
     acm_certificate_arn      = var.acm_certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
-}
-
-# 3. Attach Policy to S3 Bucket allowing ONLY CloudFront OAC to read files
-resource "aws_s3_bucket_policy" "allow_cloudfront" {
-  bucket = var.s3_bucket_id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
-        Effect    = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action   = "s3:GetObject"
-        Resource = "${var.s3_bucket_arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.this.arn
-          }
-        }
-      }
-    ]
-  })
 }
