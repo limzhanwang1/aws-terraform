ec588c843d26795a94af087b26237b7949992214
limzhanwang1 <lim.zhanwang@gmail.com>
2026-08-01 21:49:32 +0800

    Create Cloudfront #fix for s3 variable

--- Landing Page AWS/output.tf
+++ Landing Page AWS/output.tf
@@ -1,4 +1,4 @@
-output "landing_page_url" {
+output "s3_landing_page_url" {
   description = "The URL of the deployed S3 website"
-  value       = module.landing_page_site.website_endpoint
+  value       = module.s3_landing_page.website_endpoint
 }
